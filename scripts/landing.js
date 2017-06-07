445018a6d77ca895e2bc81123313dcbc9b826804
Your Name <you@example.com>
2017-06-07 10:41:44 -0700

    Added dom events checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,5 @@
-var revealPoint = function(){
-  var points = document.getElementsByClassName('point');
+var points = document.getElementsByClassName('point');
+var revealPoint = function(points){
   for (var i = 0; i < points.length; i++){
     points[i].style.transform = "translate3d(0, 0, 0)";
     points[i].style.transition = "transform 0.8s";
@@ -7,3 +7,18 @@ var revealPoint = function(){
 
   }
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+
+  window.addEventListener('scroll', function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+         revealPoint(points);
+       }
+   });
+}
